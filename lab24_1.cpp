c3c59a2b9b1ff95b4b0eeb4f5c4a050ae54613c6
saranrunran <peemai.saranporn@gmail.com>
2026-03-07 23:50:54 +0700

    lab24_1completed

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -64,3 +64,49 @@ void List::append(int d){
 }
 
 //Write List::remove() here
+
+void List::remove(int indx) {
+	Node *current = root;
+	Node *prev = nullptr;
+	for (int i = 0; i < indx; i++) {
+		prev = current;
+		current = current->next;
+	}
+	if (indx == 0) {
+		root = current->next;
+	} else {
+		prev->next = current->next;
+	}
+	delete current;
+	size--;
+}
+
+
+int main() {
+	List myList = {0,0};
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
+	myList.show();
+	cout << endl;
+
+	myList.insert(29,2);
+	myList.insert(33,3);
+	myList.insert(77,5);
+	myList.insert(69,0);
+	myList.show();
+	cout << "\n"; 
+
+	myList.remove(2);
+	myList.show();
+	cout << "\n"; 
+	myList.remove(4);
+	myList.show();
+	cout << "\n";
+	myList.remove(0);
+	myList.show();
+	cout << "\n";
+}
